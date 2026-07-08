6cf82e939784269a4d47acac99f31050dbac2a33
Dev Dipankar Bera <dbera@charlotte.edu>
2026-07-08 14:41:34 -0400

    Error correction for Hero Page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
 function Hero(){
 
-    return 
+    return(
     <section className = "hero">
-        <h1>Hi, My name is Dev Dipankar Bera</h1>
+        <p className='code-tag'>const developer = "Dev Dipankar Bera";</p>
+        <h1>Hi, I'm Dev Dipankar Bera👋</h1>
+        <p></p>
         <p> I'm a front-end developer building clean, modern web experiences.</p>
-        <a href="projects" className="cta-buttonn">See my work</a>
+        <a href="projects" className="cta-button">See my work</a>
     </section>
+    )
 }
-export default Hero
 
+export default Hero

2af00b1d178f833aec78060208f53f57c67d2eca
Dev Dipankar Bera <dbera@charlotte.edu>
2026-07-08 11:11:09 -0400

    Changed hero page and added some styles for it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
 function Hero(){
 
+    return 
+    <section className = "hero">
+        <h1>Hi, My name is Dev Dipankar Bera</h1>
+        <p> I'm a front-end developer building clean, modern web experiences.</p>
+        <a href="projects" className="cta-buttonn">See my work</a>
+    </section>
 }
 export default Hero
 
